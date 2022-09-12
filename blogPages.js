6781dfc4d2789fbf7340ecc3ee883755ffe077ac
SoJoes <113204171+SoJoes@users.noreply.github.com>
2022-09-12 15:15:42 +0100

    Add files via upload

--- blogPages.js
+++ blogPages.js
@@ -1,18 +1,32 @@
-function myFunc(){
-    alert("You clicked me");
+function getOffset(el) {
+  const rect = el.getBoundingClientRect();
+  return {
+    left: rect.left + window.scrollX,
+    top: rect.top + window.scrollY
+  };
 }
 
-let navbar = document.getElementById("topnav");
-let navPos = navbar.getBoundingClientRect().top;
+var navbar = "O";
+var sticky = 10;
 
-window.addEventListener("scroll", e => {
-  var viewportHeight = window.innerHeight;
-  var scrollPos = window.scrollY;
-  if (scrollPos > navPos) {
-    navbar.classList.add('sticky');
-    header.classList.add('navbarOffsetMargin');
+function declareVars() {
+    // Get the navbar
+    navbar = document.getElementById("topnav");
+
+    // Get the offset position of the navbar
+    sticky = getOffset(navbar).top;
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+
+  if (window.pageYOffset >= sticky) {
+    document.getElementById('topnav').style.position = "fixed";
+    document.getElementById('topnav').style.top = "0px";
   } else {
-    navbar.classList.remove('sticky');
-    header.classList.remove('navbarOffsetMargin');
+    document.getElementById('topnav').style.position = "relative";
   }
-});
+}
